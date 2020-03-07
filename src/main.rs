a5830e6e6fd19fee3cd5b382b2c5df7e5a07aa7e
Adarsh Krishnan <adarshk7@gmail.com>
2020-03-07 13:20:12 +0200

    Implement printing hello world

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,27 @@
 
 use core::panic::PanicInfo;
 
+static TEXT: &[u8] = b"Hello World!";
+
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default
+
+    // This number is a magic number which points to the location in memory where
+    // we can write data that will be displayed as text. This contains a series of
+    // couple of bytes, the first one being the character and the second being color.
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in TEXT.iter().enumerate() {
+        // We need this unsafe block because we're dealing with a raw pointer.
+        unsafe{
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb; // 0xb is light cyan.
+        }
+    }
+
+    // We loop because there's nothing else to do here.
     loop {}
 }
 
